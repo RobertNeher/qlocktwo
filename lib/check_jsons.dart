9f1bdc86cbb904878763c89155486d7c68f92726
rneher <robert.neher@bartscher-hasenaecker.de>
2025-05-30 19:33:52 +0200

    Bugs in jsons removed

--- lib/check_jsons.dart
+++ lib/check_jsons.dart
@@ -2,21 +2,20 @@ import 'dart:convert';
 import 'dart:io';
 
 void main(List<String> args) async {
-  Map<String, dynamic> settings =
-      json.decode(
-        File('assets/settings/settings.json').readAsStringSync(),
-      )['settings'];
   Map<String, dynamic> language = json.decode(
     File('assets/settings/${args[0]}.json').readAsStringSync(),
   );
-
-  String hourMask = '';
 
   print('${args[0]} ${(language['qlockTwoChars']).length}');
 
   for (int hour = 0; hour < 13; hour += 1) {
     print(
       'Hour $hour: ${(language['hoursMapping'][hour][hour.toString()]).length}',
+    );
+  }
+  for (int minute = 0; minute < 60; minute += 5) {
+    print(
+      'Minute $minute: ${(language['fiveMinutesMapping'][(minute / 5).round()][minute.toString()]).length}',
     );
   }
 }
